6e4701e4e74501300de38763c018f6a4989659c2
Nikodem <N.Kosowski@zset.leszno.pl>
2026-02-06 13:45:28 +0100

    tak

--- header.php
+++ header.php
@@ -16,33 +16,33 @@
                 
         
                 <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
-                <li><a href=""><i></i> Moje rezerwacje</a></li>
-                
+                <li><a href="my_reservations.php"><i class="fas fa-calendar-check"></i> Moje rezerwacje</a></li>
+                <?php endif; ?>
                 
                 <?php if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1): ?>
                 <li><a href="admin.php"><i class="fas fa-cog"></i> Panel admina</a></li>
                 <?php endif; ?>
-                <?php endif; ?>
                 
                 <li><a href="#kontakt"><i class="fas fa-phone"></i> Kontakt</a></li>
             </ul>
-            <div class="auth-buttons">
-                <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
-                    <span class="">
-                        <i class=""></i> 
-                        <?php echo htmlspecialchars($_SESSION['username']); ?>
-                    </span>
-                    <a href="logout.php" class="">
-                        <i class=""></i> Wyloguj
-                    </a>
-                <?php else: ?>
-                    <a href="login.php" class="">
-                        <i class=""></i> Zaloguj
-                    </a>
-                    <a href="register.php" class="">
-                        <i class=""></i> Zarejestruj
-                    </a>
-                <?php endif; ?>
+      <div class="auth-buttons">
+    <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+        <span class="">
+            <i class=""></i> 
+            <?php echo htmlspecialchars($_SESSION['username']); ?>
+        </span>
+        <a href="logout.php" class="">
+            <i class=""></i> Wyloguj
+        </a>
+    <?php else: ?>
+        <a href="login.php" class="">
+            <i class=""></i> Zaloguj
+        </a>
+        <a href="register.php" class="">
+            <i class=""></i> Zarejestruj
+        </a>
+    <?php endif; ?>
+</div>
             </div>
         </nav>
     </div>

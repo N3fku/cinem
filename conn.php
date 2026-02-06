4079ddad44a591d40afa1fb2fe1033e7e5bcb02e
Nikodem <N.Kosowski@zset.leszno.pl>
2026-02-06 13:06:33 +0100

    dodanie polacznenia

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_base = "kino67";
+
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_base);
+
+
+if (!$conn) {
+    die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
+}
+
+
+mysqli_set_charset($conn, "utf8mb4");
+?>

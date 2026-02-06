d211a68933f3f6fed04d857e90c85ba0a98c1b6c
Nikodem <N.Kosowski@zset.leszno.pl>
2026-02-06 13:21:58 +0100

    dodanie headera

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-header.css">
+    <title>header</title>
+</head>
+<body>
+     <div class="container">
+        <nav>
+            <a href="index.php" class="logo">KINO<span>67</span></a>
+            <ul class="nav-links">
+                <li><a href="index.php"><i class="fas fa-home"></i> Strona główna</a></li>
+                <li><a href="movies.php"><i class="fas fa-film"></i> Repertuar</a></li>
+                
+        
+                <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+                <li><a href=""><i></i> Moje rezerwacje</a></li>
+                
+                
+                <?php if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1): ?>
+                <li><a href="admin.php"><i class="fas fa-cog"></i> Panel admina</a></li>
+                <?php endif; ?>
+                <?php endif; ?>
+                
+                <li><a href="#kontakt"><i class="fas fa-phone"></i> Kontakt</a></li>
+            </ul>
+            <div class="auth-buttons">
+                <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+                    <span class="">
+                        <i class=""></i> 
+                        <?php echo htmlspecialchars($_SESSION['username']); ?>
+                    </span>
+                    <a href="logout.php" class="">
+                        <i class=""></i> Wyloguj
+                    </a>
+                <?php else: ?>
+                    <a href="login.php" class="">
+                        <i class=""></i> Zaloguj
+                    </a>
+                    <a href="register.php" class="">
+                        <i class=""></i> Zarejestruj
+                    </a>
+                <?php endif; ?>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cinema</title>
+    <link rel="stylesheet" href="style-index.css">
+</head>
+<body>
+    <?php 
+    include 'header.php'; 
+    ?>
+
+    <section class="hero">
+        <h1>Doświadcz Magii Kina</h1>
+        <p>Najnowsze premiery, komfortowe sale i niezapomniane wrażenia. Zarezerwuj bilety online już teraz!</p>
+        <a href="movies.php" class="btn btn-primary">Zobacz repertuar</a>
+</body>
+</html>

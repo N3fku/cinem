5a565516331ed2a687ce3c033cf986a4ad9ef18f
Nikodem <N.Kosowski@zset.leszno.pl>
2026-02-10 15:21:56 +0100

    zamiany

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ if (session_status() == PHP_SESSION_NONE) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
